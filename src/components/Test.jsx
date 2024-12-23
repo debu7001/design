1d5654efb5ab96f0ea00641fcf45f4528d58b5f0
debu7001 <debanjan.khan@excellisit.net>
2024-12-23 12:56:30 +0530

    pop up

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap'
 import { IoFootballOutline } from "react-icons/io5";
 import { MdSportsCricket } from "react-icons/md";
@@ -8,21 +8,63 @@ import { MdOutlineSportsKabaddi } from "react-icons/md";
 import { FaSearchPlus } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 import { MdLock } from "react-icons/md";
+import { FaHome } from "react-icons/fa";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import { useRef } from 'react';
 
 const Test = () => {
+    const popUpRef = useRef(null);
     const [isOpen, setIsOpen] = useState(false);
+    const [popUp,setPopUp]=useState(false)
+
+        useEffect(() => {
+            const handleClickOutside = (event) => {
+               
+                if (popUpRef.current && !popUpRef.current.contains(event.target)) {
+                    setPopUp(false);
+                }
+            };
+    
+
+            document.addEventListener("mousedown", handleClickOutside);
+    
+  
+            return () => {
+                document.removeEventListener("mousedown", handleClickOutside);
+            };
+        }, []); 
 
     const toggleSearch = () => {
         setIsOpen((prev) => !prev);
+    };
+
+    const togglePopUp = () => {
+        setPopUp((prev) => !prev);
     };
     return (
         <>
             <div className='header'>
                 <Row className='spaceBetween'>
 
-                    <Col sm={6} style={{ display: 'flex' }}><img className='logo' src="https://sitethemedata.com/sitethemes/d247.com/front/logo.png" /></Col>
+                    <Col sm={6} style={{ display: 'flex' }}><FaHome size={30} color='white' style={{ marginRight: '5px' }} /><img className='logo' src="https://sitethemedata.com/sitethemes/d247.com/front/logo.png" /></Col>
 
-                    <Col sm={6}><div class="user-balance ms-1 ms-xl-3"><div><span>Balance:</span><b>1500</b></div><div><span>Exp:</span><b class="pointer">0</b><div class="dropdown"><a class="user-name ms-1 ms-xl-3 d-inline-block d-xl-none dropdown-toggle" id="react-aria2371733761-1" aria-expanded="false">Demo<i class="fas fa-chevron-down ms-1"></i></a></div></div></div></Col>
+                    <Col sm={6}>
+                        <div class="user-balance ms-1 ms-xl-3">
+                            <div><span>Balance:</span><b>1500</b></div>
+                            <div className='expodemo' onClick={togglePopUp}>
+                                <span>Exp:0</span>
+                                <div className='verticallyCenter'>
+                                    <div class="dropdown "><a class="user-name ms-1 ms-xl-3 d-inline-block d-xl-none dropdown-toggle" id="react-aria2371733761-1" aria-expanded="false">Demo </a></div>
+                                    <RiArrowDropDownLine size={20} className='downArrow' />
+                                   {
+                                    popUp && ( <div className='popUp' ref={popUpRef}>
+
+                                        </div>)
+                                   }
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
 
 
                 </Row>
@@ -42,14 +84,15 @@ const Test = () => {
 
                             </div>
                             <span><FaSearchPlus onClick={toggleSearch} style={{ color: 'white' }} size={20} /></span>
+                            <marquee scrollamount="3" className="scrollTxt">Big Bash League 2024-25 is Live! Scratch to Unlock Incredible Rewards!</marquee>
                         </div>
 
                     </Col>
                 </Row>
                 <div class="scrollmenu">
-                    <a href="#home" className='nav1menu'><span className='iconWithName'><IoFootballOutline className='icon' />Valencia v Alaves</span></a>
-                    <a href="#news" className='nav1menu'><span className='iconWithName'><MdSportsCricket className='icon' />South Africa v Pakistan</span></a>
-                    <a href="#contact" className='nav1menu'><span className='iconWithName'><IoFootballOutline className='icon' />Everton v Chelsea</span></a>
+                    <a href="#home" className='nav1menu '><span className='iconWithName blinking-text'><IoFootballOutline className='icon' />Valencia v Alaves</span></a>
+                    <a href="#news" className='nav1menu'><span className='iconWithName blinking-text'><MdSportsCricket className='icon' />South Africa v Pakistan</span></a>
+                    <a href="#contact" className='nav1menu'><span className='iconWithName blinking-text'><IoFootballOutline className='icon' />Everton v Chelsea</span></a>
 
 
 
@@ -80,14 +123,14 @@ const Test = () => {
                 <span className='oneXtwo'>2</span>
             </div>
             <div class="container">
-               
-                   
-                    <div class="box locked"><div className='lock'><MdLock color='white'/></div></div>
-                    <div class="box"></div>
-                
+
+
+                <div class="box locked"><div className='lock'><MdLock color='white' size={20} /></div></div>
+                <div class="box"></div>
+
                 <div class="box blue"></div>
                 <div class="box pink"></div>
-                <div class="box"><div className='lock'><MdLock color='white'/></div>chNGES</div>
+                <div class="box"><div className='lock'><MdLock color='white' size={20} /></div></div>
                 <div class="box locked"></div>
             </div>
 
